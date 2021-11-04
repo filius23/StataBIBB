757ee0310129d529f7e65ebcc7379ffb6b4d9352
filius23 <filser-a@gmx.de>
2021-11-04 18:11:33 +0100

    kap2 ready

--- 00_r_exploration.R
+++ 00_r_exploration.R
@@ -8,7 +8,7 @@ baua %>% select(where(function(x) sum(is.na(x)) / length(x) < 0.5))
 
 baua2 %>% select(where(function(x) sum(as.numeric(x) == 9999) > 0 ))
 
-xt <- baua2 %>% summarise(across(everything(), ~sum(as.numeric(.x) %in% c(9999,99999)) > 0 ))  %>% 
+xt <- baua2 %>% summarise(across(everything(), ~sum(as.numeric(.x) %in% c(98)) > 0 ))  %>% 
   t(.) %>% data.frame(.) %>% rownames_to_column(.,var = "var") %>% janitor::clean_names() %>% tibble() %>% filter(x)
 
 
@@ -16,6 +16,13 @@ labs <- baua %>% map(.,~attributes(.x)$label) %>% bind_rows(.) %>%
   t(.) %>% data.frame() %>% rownames_to_column(.,var = "var") %>% janitor::clean_names() %>% tibble() 
 
 
+
+xt <- baua2 %>% summarise(across(everything(), ~sum(as.numeric(.x) %in% c(9999)) > 0 ))  %>% 
+  t(.) %>% data.frame(.) %>% rownames_to_column(.,var = "var") %>% janitor::clean_names() %>% tibble() %>% filter(x)
+
+
 xt %>% left_join(labs, by ="var")
 
 
+baua2 %>% summarise(across(everything(), ~length(unique(.x)  ))  %>% 
+  t(.) %>% data.frame(.) %>% rownames_to_column(.,var = "var") %>% janitor::clean_names() %>% tibble()
