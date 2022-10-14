4155a01e8b0e39229aa698f23769884c481834ec
Andreas Filser <filser-a@gmx.de>
2022-10-14 11:43:19 +0200

    update gitignore

--- 00_r_exploration.R
+++ 00_r_exploration.R
@@ -1,4 +1,4 @@
-if(Sys.getenv("USERNAME") == "Filser" ) .libPaths("D:/R-library4")  # set library
+if(Sys.getenv("USERNAME") == "filse" ) .libPaths("D:/R-library4")  # set library
 library(tidyverse)
 baua <- haven::read_dta("D:/Datenspeicher/BIBB_BAuA/BIBBBAuA_2018_suf1.0.dta")
 baua2 <- readstata13::read.dta13("D:/Datenspeicher/BIBB_BAuA/BIBBBAuA_2018_suf1.0.dta",convert.factors = F)
@@ -29,7 +29,7 @@ xt <- baua2 %>% summarise(across(everything(), ~sum(as.numeric(.x) %in% c(9999))
   t(.) %>% data.frame(.) %>% rownames_to_column(.,var = "var") %>% janitor::clean_names() %>% tibble() %>% filter(x)
 
 
-
+install.packages(c('gganimate','sf'),lib = "D:/R-library4")
 
   
 
